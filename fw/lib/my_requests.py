967c53978688a0e7d64bca6ce3d315d699a3eac2
Alexander K <nafanyabr@gmail.com>
2022-12-06 22:30:10 +0300

    Add env support

--- fw/lib/my_requests.py
+++ fw/lib/my_requests.py
@@ -1,6 +1,7 @@
 import allure
 import requests
 from lib.logger import Logger
+from lib.environment import ENV_OBJECT
 
 class MyRequests():
     @staticmethod
@@ -26,7 +27,7 @@ class MyRequests():
     @staticmethod
     def _send(url: str, data: dict, headers: dict, cookies: dict, method: str):
 
-        url = f'https://playground.learnqa.ru/api{url}'
+        url = f'{ENV_OBJECT.get_base_url()}{url}'
 
         if headers is None:
             headers = {}
